69063f21698f698b014924f9ab5d65a28d74cd50
buyrak <siyahmerkez@gmail.com>
2022-03-18 03:43:39 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 window.onload = function (){
 
-    let hours = '00'
-    let minutes = '00'
-    let seconds = '00'
-    let minseconds = '000'
+    let hour = '0'
+    let minute = '0'
+    let second = '0'
+    let minsecond = '0'
 
-    let hour = document.getElementById('hour')
-    let minute = document.getElementById('minute')
-    let second = document.getElementById('second')
-    let minsecond = document.getElementById('minsecond')
+    let hourFront = document.getElementById('hour')
+    let minuteFront = document.getElementById('minute')
+    let secondFront = document.getElementById('second')
+    let minsecondFront = document.getElementById('minsecond')
     let start = document.getElementById('start')
 
     let interval
@@ -16,30 +16,40 @@ window.onload = function (){
     start.onclick = function (){
         clearInterval(interval)
         interval = setInterval(startTimer,10)
+        document.getElementById('start').innerHTML = '<i class="bi bi-pause"></i> Stop'
+        document.getElementById('start').id = 'stop'
     }
-
     function startTimer () {
-        minseconds++;
+        minsecond++;
 
-        if(minseconds <= 9){
-            minsecond.innerHTML = "0" + minseconds;
+        if(minsecond <= 9){
+            minsecondFront.innerHTML = "0" + minsecond;
         }
 
-        if (minseconds > 9){
-            minsecond.innerHTML = minseconds;
+        if (minsecond > 9){
+            minsecondFront.innerHTML = minsecond;
 
         }
 
-        if (minseconds > 99) {
-            console.log("seconds");
-            seconds++;
-            second.innerHTML = "0" + seconds;
-            minseconds = 0;
-            minsecond.innerHTML = "0" + 0;
+        if (minsecond >= 99) {
+            minsecond = 0;
+            second++;
+            second >= '10' ? secondFront.innerHTML = second : secondFront.innerHTML = '0' + second
+            minsecondFront.innerHTML = '0' + 0;
         }
 
-        if (seconds > 9){
-            second.innerHTML = seconds;
+        if (second >= 59){
+            second = 0;
+            minute++;
+            minute >= '10' ? minuteFront.innerHTML = minute : minuteFront.innerHTML = '0' + minute
+            minsecondFront.innerHTML = '0' + 0;
+        }
+
+        if (minute >= 60){
+            minute = 0;
+            hour++;
+            hour >= '10' ? hourFront.innerHTML = hour : hourFront.innerHTML = '0' + hour
+            minuteFront.innerHTML = '0' + 0;
         }
 
     }
