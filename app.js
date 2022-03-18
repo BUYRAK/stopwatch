9391c1da40aefdfa69fbf6c1846c844de19e7319
buyrak <siyahmerkez@gmail.com>
2022-03-18 22:44:13 +0300

    Initial commit

--- app.js
+++ app.js
@@ -10,15 +10,38 @@ window.onload = function (){
     let secondFront = document.getElementById('second')
     let minsecondFront = document.getElementById('minsecond')
     let start = document.getElementById('start')
+    let split = document.getElementById('split')
+    let stop = document.getElementById('stop')
+    let reset = document.getElementById('reset')
 
     let interval
 
-    start.onclick = function (){
+    start.onclick = (() => {
         clearInterval(interval)
         interval = setInterval(startTimer,10)
         document.getElementById('start').innerHTML = '<i class="bi bi-pause"></i> Stop'
         document.getElementById('start').id = 'stop'
+    })
+
+    stop.onclick = function (){
+        clearInterval(interval)
+        document.getElementById('stop').innerHTML = '<i class="bi bi-timer"></i> Start'
+        document.getElementById('stop').id = 'start'
     }
+    reset.onclick = function (){
+        clearInterval(interval)
+        hour = '0'
+        minute = '0'
+        second = '0'
+        minsecond = '0'
+        hourFront.innerHTML = '0' + hour;
+        minuteFront.innerHTML = '0' + minute;
+        secondFront.innerHTML = '0' + second;
+        minsecondFront.innerHTML = '0' + minsecond;
+        document.getElementById('stop').innerHTML = '<i class="bi bi-timer"></i> Start'
+        document.getElementById('stop').id = 'start'
+    }
+
     function startTimer () {
         minsecond++;
 
